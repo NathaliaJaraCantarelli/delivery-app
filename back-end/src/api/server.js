7e3db824d30bc2aaa748e57cb9a443f0293123c7
Nathalia <nathalia.cantarelli@edu.pucrs.br>
2023-05-13 22:06:07 -0300

    FIX: coigindo o server

--- back-end/src/api/server.js
+++ back-end/src/api/server.js
@@ -1,5 +1,13 @@
-const port = process.env.PORT || 3001;
-const app = require('./app');
+// const port = process.env.PORT || 3001;
+// const app = require('./app');
 
-app.listen(port);
+// app.listen(port);
+// console.log(`Api rodando na porta ${port}`);
+
+const port = process.env.PORT || 3001;
+const App = require('./app');
+
+const app = new App();
+
+app.server.listen(port);
 console.log(`Api rodando na porta ${port}`);
